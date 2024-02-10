adb418e11878013b10207a87ed3c12dc66a374fd
hayao <hayao@fascode.net>
2024-02-11 07:35:45 +0900

    Fix conflict

--- src/app/(main)/(nav)/layout.tsx
+++ src/app/(main)/(nav)/layout.tsx
@@ -1,6 +1,5 @@
-import Nav from "./Nav"
-import PictureDialog from "~/features/note/PictureDialog"
 import NoteDialog from "~/features/note/NoteDialog"
+import PictureDialog from "~/features/note/PictureDialog"
 import Nav from "./Nav"
 
 export default function NavLayout({ children }: { children: React.ReactNode }) {
